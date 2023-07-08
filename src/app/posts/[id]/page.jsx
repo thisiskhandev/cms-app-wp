e4def6a372485837f4db25828399f1cdfd7f9712
thisiskhandev <hassankhan427726@gmail.com>
2023-07-09 02:13:14 +0500

    Dynamic meta set in Posts dynamic page

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,23 @@
+export async function generateMetadata({ params }) {
+  let pageTitle = params.id;
+  pageTitle = pageTitle.replaceAll("-", " ");
+  let capsLetter = pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
+  //   console.log(capsLetter);
+  return {
+    title: capsLetter,
+  };
+}
+
+export default function DynamicPostsRoute({ params }) {
+  console.log(params.id);
+  return (
+    <div>
+      <h1>
+        Page Title is{" "}
+        <code>
+          <mark>{params.id}</mark>
+        </code>
+      </h1>
+    </div>
+  );
+}
